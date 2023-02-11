f52cc1ab19833e9b6d3a480676db2f7f22652d92
sanjanaak556 <117841426+sanjanaak556@users.noreply.github.com>
2023-02-11 18:51:00 +0530

    commited

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /** @format */
 
-let arr = [
+let students = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
   { id: 3, name: "karen", age: "19", marks: 35 },
@@ -8,15 +8,29 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  pass=students.filter(student=>student["marks"]>=50)
+  pass1=pass.map(name=>name)
+  console.log(pass1);
+
 }
+PrintStudentswithMap()
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  pass=students.filter(student=>student["marks"]>=50)
+  .forEach(stud=>console.log(`${stud["name"]} is ${stud["marks"]}`));
+
 }
+PrintStudentsbyForEach()
 
 function addData() {
   //Write your code here, just console.log
+let obj={id:4, name:"susan", age:"20", marks:45}
+students.push(obj)
+console.log(students);
+
 }
+addData()
 
 function removeFailedStudent() {
   //Write your code here, just console.log
@@ -25,3 +39,5 @@ function removeFailedStudent() {
 function concatenateArray() {
   //Write your code here, just console.log
 }
+
+PrintStudentsbyForEach();
